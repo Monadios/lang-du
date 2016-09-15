3399703f7e8c8f4db3b0f3e4ac890d55b039610c
Monadios <gfalkenthros@gmail.com>
2016-09-15 11:41:32 +0200

    moved code into proper functions

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import os
+import re
 
 phoneme_dir = "phonemes"
 
-def make_file(word,tone):
+def get_phoneme(word,tone):
     return phoneme_dir + "/" + word + tone + ".mp3"
 
 def play_file(f):
     os.system("mplayer " + f)
 
+def split_phoneme(phon):
+    match = re.match(r"([a-z]+)([0-9]+)", phon, re.I)
+    return match.groups()
+
+def play_word(word):
+    base,tone = split_phoneme(word)
+    f = get_phoneme(base,tone)
+    play_file(f)
+
+
+def play_sentence(sen):
+    #TODO
+    # Add more sophisticated rules to playback
+    # i.e Tone changing rules, smoother transitions between words, etc
+    
+    [play_word(w) for w in sen.split(" ")]
+
 if __name__ == "__main__":
-    ni3 = make_file("ni", "3")
-    hao3 = make_file("hao", "3")
-    play_file(ni3)
-    play_file(hao3)
+    sentence = "ni3 hao3"
+    play_sentence(sentence)
